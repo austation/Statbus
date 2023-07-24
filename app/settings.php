52f7b808d45169a44e6100e16fb1316980b37930
Nick Farley <nick@nfreader.net>
2023-07-24 15:38:13 -0400

    Change settings.php require strategy

--- app/settings.php
+++ app/settings.php
@@ -2,20 +2,19 @@
 
 $settings = require __DIR__ . '/defaults.php';
 
-//Check for a development environment config file, or fallback to the production
-//file, if it exists.
-if (isset($_ENV['APP_ENV'])) {
-    if (file_exists(__DIR__ . '/conf/' . strtolower($_ENV['APP_ENV']) . '.php')) {
-        require_once __DIR__ . '/conf/' . strtolower($_ENV['APP_ENV']) . '.php';
-    }
-} else {
-    if (file_exists(__DIR__ . '/conf/prod.php')) {
-        require_once __DIR__ . '/conf/prod.php';
-    }
+if(file_exists(__DIR__ . '/conf/local.php')) {
+    require_once(__DIR__ . '/conf/local.php');
+} elseif (file_exists(__DIR__ . '/conf/dev.php')) {
+    require_once(__DIR__ . '/conf/dev.php');
+} elseif (file_exists(__DIR__ . '/conf/test.php')) {
+    require_once(__DIR__ . '/conf/test.php');
+} elseif (file_exists(__DIR__ . '/conf/prod.php')) {
+    require_once(__DIR__ . '/conf/prod.php');
 }
 
 //TODO: Move this to the container?
 require_once __dir__  . "/version.php";
+
 $settings['app']['version'] = VERSION_MAJOR . '.' . VERSION_MINOR . '.' . VERSION_PATCH . VERSION_TAG;
 
 if (file_exists(__DIR__ . '/conf/servers.json')) {
