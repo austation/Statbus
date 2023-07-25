696cff0094807646a40371557590824689cb9dc2
Nick Farley <nick@nfreader.net>
2023-07-25 10:20:05 -0400

    Add trailing slash middleware

--- app/middleware.php
+++ app/middleware.php
@@ -2,10 +2,12 @@
 
 use Slim\App;
 use Slim\Views\TwigMiddleware;
+use Middlewares\TrailingSlash;
 
 return function (App $app) {
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
     $app->add(TwigMiddleware::class);
+    $app->add(new TrailingSlash(true));
     $app->addErrorMiddleware(true, true, true);
 };
