498488efb0edbb705f4cfbc0421e33ee58547967
Nick Farley <nick@nfreader.net>
2023-08-04 12:36:46 -0400

    Version bump + footer badge

--- app/version.php
+++ app/version.php
@@ -1,7 +1,7 @@
 <?php
 
 define('VERSION_MAJOR', 1);
-define('VERSION_MINOR', 0);
-define('VERSION_PATCH', 3);
+define('VERSION_MINOR', 2);
+define('VERSION_PATCH', 0);
 
 define('VERSION_TAG', '');
