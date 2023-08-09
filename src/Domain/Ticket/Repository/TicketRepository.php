5d34ad8a44d4345d5026d25ecdb1c531951a278f
Nick Farley <nick@nfreader.net>
2023-08-09 16:52:20 -0400

    Optimize and clean up ticket queries

--- src/Domain/Ticket/Repository/TicketRepository.php
+++ src/Domain/Ticket/Repository/TicketRepository.php
@@ -9,54 +9,47 @@ class TicketRepository extends Repository
 {
     public ?string $entityClass = Ticket::class;
 
-    public function getTicketsByCkey(string $ckey, int $page = 1, int $per_page = 60): self
+    private array $columns = [
+      't.id',
+      't.server_ip as serverIp',
+      't.server_port as port',
+      't.round_id as `round`',
+      't.ticket',
+      't.action',
+      't.message',
+      't.timestamp',
+      't.recipient as r_ckey',
+      't.sender as s_ckey',
+      'r.rank as r_rank',
+      's.rank as s_rank',
+      '(SELECT `action` FROM ticket WHERE id = c.last_id LIMIT 1) as `status`',
+      ' c.replies as `replies`',
+      't.urgent',
+    ];
+
+    private array $joins = [
+      'LEFT JOIN `admin` AS r ON r.ckey = t.recipient',
+      'LEFT JOIN `admin` AS s ON s.ckey = t.sender',
+      'LEFT JOIN (SELECT round_id, ticket, COUNT(id) as `replies`, max(id) as `last_id` FROM ticket GROUP BY round_id, ticket) as c on (c.round_id = t.round_id and c.ticket = t.ticket)'
+    ];
+
+    private array $where = [
+      't.action = "Ticket Opened"',
+      't.round_id != 0'
+    ];
+
+    public function getTickets(int $page = 1, int $per_page = 60): self
     {
-        $this->setPages((int) ceil($this->db->cell(
-            "SELECT
-          count(t.id) 
-          FROM ticket t
-          WHERE t.action = 'Ticket Opened' 
-          AND (t.recipient = ? OR t.sender = ?);",
-            $ckey,
-            $ckey
-        ) / $per_page));
+        $cols = implode(",\n", $this->columns);
+        $joins = implode("\n", $this->joins);
+        $where = implode("\n AND ", $this->where);
+        $query = sprintf("SELECT %s FROM ticket t %s \nWHERE %s
+        ORDER BY t.timestamp DESC LIMIT ?, ?", $cols, $joins, $where);
+        $pagesQuery = sprintf("SELECT count(t.id) FROM ticket t WHERE %s", $where);
+        $this->setPages((int) ceil($this->db->cell($pagesQuery) / $per_page));
         $this->setResults(
             $this->db->run(
-                "SELECT 
-                t.id,
-                t.server_ip as serverIp, 
-                t.server_port as port,
-                t.round_id as `round`,
-                t.ticket,
-                t.action,
-                t.message,
-                t.timestamp,
-                t.recipient as r_ckey,
-                t.sender as s_ckey,
-                r.rank as r_rank,
-                s.rank as s_rank,
-                (SELECT `action` 
-                  FROM ticket 
-                  WHERE t.ticket = ticket AND t.round_id = round_id 
-                  ORDER BY id DESC LIMIT 1)
-                as `status`,
-                (SELECT COUNT(id) 
-                  FROM ticket 
-                  WHERE t.ticket = ticket 
-                  AND t.round_id = round_id) 
-                as `replies`,
-                t.urgent
-                FROM ticket t
-                LEFT JOIN `admin` AS r ON r.ckey = t.recipient	
-                LEFT JOIN `admin` AS s ON s.ckey = t.sender
-                WHERE t.action = 'Ticket Opened'
-                AND (t.recipient = ? OR t.sender = ?)
-                AND t.round_id != 0
-                GROUP BY t.id
-                ORDER BY `timestamp` DESC
-                LIMIT ?, ?",
-                $ckey,
-                $ckey,
+                $query,
                 ($page * $per_page) - $per_page,
                 $per_page
             ),
@@ -64,49 +57,20 @@ class TicketRepository extends Repository
         return $this;
     }
 
-    public function getTickets(int $page = 1, int $per_page = 60): self
+    public function getTicketsByCkey(string $ckey, int $page = 1, int $per_page = 60): self
     {
-        $this->setPages((int) ceil($this->db->cell(
-            "SELECT
-          count(t.id) 
-          FROM ticket t
-          WHERE t.action = 'Ticket Opened' 
-          "
-        ) / $per_page));
+        $cols = implode(",\n", $this->columns);
+        $joins = implode("\n", $this->joins);
+        $where = implode("\n AND ", [...$this->where, '(t.recipient = ? OR t.sender = ?)']);
+        $pagesQuery = sprintf("SELECT count(t.id) FROM ticket t WHERE %s", $where);
+        $query = sprintf("SELECT %s FROM ticket t %s \nWHERE %s
+        ORDER BY t.timestamp DESC LIMIT ?, ?", $cols, $joins, $where);
+        $this->setPages((int) ceil($this->db->cell($pagesQuery, $ckey, $ckey) / $per_page));
         $this->setResults(
             $this->db->run(
-                "SELECT 
-                t.id,
-                t.server_ip as serverIp, 
-                t.server_port as port,
-                t.round_id as `round`,
-                t.ticket,
-                t.action,
-                t.message,
-                t.timestamp,
-                t.recipient as r_ckey,
-                t.sender as s_ckey,
-                r.rank as r_rank,
-                s.rank as s_rank,
-                (SELECT `action` 
-                  FROM ticket 
-                  WHERE t.ticket = ticket AND t.round_id = round_id 
-                  ORDER BY id DESC LIMIT 1)
-                as `status`,
-                (SELECT COUNT(id) 
-                  FROM ticket 
-                  WHERE t.ticket = ticket 
-                  AND t.round_id = round_id) 
-                as `replies`,
-                t.urgent
-                FROM ticket t
-                LEFT JOIN `admin` AS r ON r.ckey = t.recipient	
-                LEFT JOIN `admin` AS s ON s.ckey = t.sender
-                WHERE t.action = 'Ticket Opened'
-                AND t.round_id != 0
-                GROUP BY t.id
-                ORDER BY `timestamp` DESC
-                LIMIT ?, ?",
+                $query,
+                $ckey,
+                $ckey,
                 ($page * $per_page) - $per_page,
                 $per_page
             ),
@@ -116,48 +80,16 @@ class TicketRepository extends Repository
 
     public function getTicketsForRound(int $round, int $page = 1, int $per_page = 60): self
     {
-        $this->setPages((int) ceil($this->db->cell(
-            "SELECT
-          count(t.id) 
-          FROM ticket t
-          WHERE t.action = 'Ticket Opened'
-          AND t.round_id = ?",
-            $round
-        ) / $per_page));
+        $cols = implode(",\n", $this->columns);
+        $joins = implode("\n", $this->joins);
+        $where = implode("\n AND ", [...$this->where, 't.round_id = ?']);
+        $pagesQuery = sprintf("SELECT count(t.id) FROM ticket t WHERE %s", $where);
+        $query = sprintf("SELECT %s FROM ticket t %s \nWHERE %s
+      ORDER BY t.timestamp DESC LIMIT ?, ?", $cols, $joins, $where);
+        $this->setPages((int) ceil($this->db->cell($pagesQuery, $round) / $per_page));
         $this->setResults(
             $this->db->run(
-                "SELECT 
-                t.id,
-                t.server_ip as serverIp, 
-                t.server_port as port,
-                t.round_id as `round`,
-                t.ticket,
-                t.action,
-                t.message,
-                t.timestamp,
-                t.recipient as r_ckey,
-                t.sender as s_ckey,
-                r.rank as r_rank,
-                s.rank as s_rank,
-                (SELECT `action` 
-                  FROM ticket 
-                  WHERE t.ticket = ticket AND t.round_id = round_id 
-                  ORDER BY id DESC LIMIT 1)
-                as `status`,
-                (SELECT COUNT(id) 
-                  FROM ticket 
-                  WHERE t.ticket = ticket 
-                  AND t.round_id = round_id) 
-                as `replies`,
-                t.urgent
-                FROM ticket t
-                LEFT JOIN `admin` AS r ON r.ckey = t.recipient	
-                LEFT JOIN `admin` AS s ON s.ckey = t.sender
-                WHERE t.action = 'Ticket Opened'
-                AND t.round_id = ?
-                GROUP BY t.id
-                ORDER BY `timestamp` DESC
-                LIMIT ?, ?",
+                $query,
                 $round,
                 ($page * $per_page) - $per_page,
                 $per_page
