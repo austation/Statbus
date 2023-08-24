88fdbb02efa419509039090b15dd090f2392f75a
Nick Farley <nick@nfreader.net>
2023-08-24 13:15:04 -0400

    Link to ticket in TGDB for admins

--- src/Controller/Tickets/TicketViewerController.php
+++ src/Controller/Tickets/TicketViewerController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Tickets;
 use App\Controller\Controller;
 use App\Domain\Ticket\Repository\TicketRepository;
 use App\Domain\Ticket\Service\GetTicketCkeys;
-use App\Exception\StatbusUnauthorizedException;
+use App\Exception\StatbusNotYourTicketException;
 use Psr\Http\Message\ResponseInterface;
 use DI\Attribute\Inject;
 
@@ -19,7 +19,16 @@ class TicketViewerController extends Controller
 
         $ticket = $this->ticketRepository->getSingleTicket($this->getArg('round'), $this->getArg('ticket'))->getResults();
         if(!$this->getUser() || false == GetTicketCkeys::isCkeyInTicket($ticket, $this->getUser()->getCkey())) {
-            throw new StatbusUnauthorizedException("You do not have permission to view this ticket", 403);
+            return $this->render('error.html.twig', [
+                'error' => new StatbusNotYourTicketException("This ticket does not belong to you", 403),
+                'class' => 'App/Exception/StatbusNotYourTicketException',
+                'code' => 403,
+                'narrow' => true,
+                'link' => $this->getUriForRoute('tgdb.ticket', [
+                    'round' => $this->getArg('round'),
+                    'ticket' => $this->getArg('ticket')
+                ])
+            ]);
         }
         return $this->render('tickets/single.html.twig', [
             'ticket' => $ticket,
