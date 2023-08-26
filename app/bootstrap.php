11a317c9fb4a09c394ee50c4321dab2dc6be15cf
Nick Farley <nick@nfreader.net>
2023-08-26 14:55:40 -0400

    Ugh missing a function

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,23 @@ function pick($list)
     } elseif (is_object($list)) {
         $list = json_decode(json_encode($list), true);
     }
-    return $list[floor(rand(0, count($list)-1))];
+    return $list[floor(rand(0, count($list) - 1))];
+}
+
+// https://stackoverflow.com/a/41910059
+function getYoutubeEmbedUrl($url)
+{
+    $shortUrlRegex = '/youtu.be\/([a-zA-Z0-9_-]+)\??/i';
+    $longUrlRegex = '/youtube.com\/((?:embed)|(?:watch))((?:\?v\=)|(?:\/))([a-zA-Z0-9_-]+)/i';
+
+    if (preg_match($longUrlRegex, $url, $matches)) {
+        $youtube_id = $matches[count($matches) - 1];
+    }
+
+    if (preg_match($shortUrlRegex, $url, $matches)) {
+        $youtube_id = $matches[count($matches) - 1];
+    }
+    return 'https://www.youtube.com/embed/' . $youtube_id ;
 }
 
 use DI\Bridge\Slim\Bridge;
