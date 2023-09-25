4381cd7ac9cad4fef9aff5333360ea96c55b4c99
Nick Farley <nick@nfreader.net>
2023-09-25 14:24:19 -0500

    Gracefully handle missing round end data

--- src/Domain/Round/Service/GetExternalRoundData.php
+++ src/Domain/Round/Service/GetExternalRoundData.php
@@ -4,6 +4,7 @@ namespace App\Domain\Round\Service;
 
 use App\Domain\Round\Data\Round;
 use App\Domain\Stat\Data\Stat;
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use Kevinrob\GuzzleCache\CacheMiddleware;
@@ -21,8 +22,12 @@ class GetExternalRoundData
             'timeout'  => 2.0,
             'handler' => $stack
         ]);
-        $request = $client->request('GET', $round->getPublicLogFile('round_end_data.json'));
-        $stat = new Stat(-1, $round->getStartDatetime(), $round->getId(), 'sb_who', 'associative', 1, (string) $request->getBody());
+        try {
+            $request = $client->request('GET', $round->getPublicLogFile('round_end_data.json'));
+        } catch (Exception $e) {
+            return new Stat(-1, $round->getStart(), $round->getId(), 'sb_who', 'associative', 1, false);
+        }
+        $stat = new Stat(-1, $round->getStart(), $round->getId(), 'sb_who', 'associative', 1, (string) $request->getBody());
         return $stat;
     }
 
@@ -52,6 +57,30 @@ class GetExternalRoundData
         } catch (\Guzzle\Http\Exception\ConnectException $e) {
             $response = json_encode((string)$e->getResponse()->getBody());
         }
+    }
+
+    public static function getLogList(Round $round): array
+    {
+        $stack = HandlerStack::create();
+        $stack->push(new CacheMiddleware(new GreedyCacheStrategy(null, 3000)), 'cache');
+
+        $client = new Client([
+            'base_uri' => 'https://tgstation13.org/',
+            'timeout'  => 2.0,
+            'handler' => $stack
+        ]);
+        $request = $client->request('GET', $round->getPublicLogFile("?index_format=json"));
+        $list = [];
+        foreach(json_decode($request->getBody(), true) as $l) {
+            if(!str_ends_with($l['name'], '.html.gz')) {
+                $l['name'] = str_replace('.gz', '', $l['name']);
+                $l['name'] = sprintf("%s/%s", $round->getPublicLogs(), $l['name']);
+                $file = explode('/', $l['name']);
+                $l['file'] = end($file);
+                $list[] = $l;
+            }
+        }
+        return $list;
     }
 
 }
