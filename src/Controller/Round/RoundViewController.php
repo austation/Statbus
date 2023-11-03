4e7831fba14f788b4378a5077abb51552e3c7663
Nick Farley <nick@nfreader.net>
2023-11-03 11:35:25 -0400

    Adds threat level information to round listings

--- src/Controller/Round/RoundViewController.php
+++ src/Controller/Round/RoundViewController.php
@@ -30,7 +30,12 @@ class RoundViewController extends Controller
         } else {
             $data = [
                 'round' => $round,
-                'stats' => $this->statRepository->getStatsForRound($round->getId(), ['antagonists','testmerged_prs','commendation','nuclear_challenge_mode']),
+                'stats' => $this->statRepository->getStatsForRound($round->getId(), [
+                    'antagonists','
+                    testmerged_prs',
+                    'commendation',
+                    'nuclear_challenge_mode',
+                ]),
                 'statlist' => $this->statRepository->listStatsForRound($round->getId()),
                 'deaths' => $this->deathRepository->getDeathsForRound($round->getId()),
 
