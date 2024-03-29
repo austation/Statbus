28cb5c83d0e8aa777b97b000dcafdca084ba037d
Nick <nick@nfreader.net>
2024-03-29 10:26:16 -0400

    Detects users who received a ban after ahelping

--- src/Controller/TGDB/Tickets/TGDBTicketViewerController.php
+++ src/Controller/TGDB/Tickets/TGDBTicketViewerController.php
@@ -4,6 +4,7 @@ namespace App\Controller\TGDB\Tickets;
 
 use App\Controller\Controller;
 use App\Domain\Ticket\Repository\TicketRepository;
+use App\Enum\TicketActions;
 use Psr\Http\Message\ResponseInterface;
 use DI\Attribute\Inject;
 
@@ -16,14 +17,21 @@ class TGDBTicketViewerController extends Controller
     {
         $ticket = $this->ticketRepository->getSingleTicket($this->getArg('round'), $this->getArg('ticket'))->getResults();
         $badges = [];
+        $triggeredBan = false;
         foreach($ticket as $t) {
             $badges[] = $t->getSenderBadge();
             $badges[] = $t->getRecipientBadge();
+            if(TicketActions::INTERACTION === $t->getAction()) {
+                if(str_contains($t->getMessage(), 'server ban') || str_contains($t->getMessage(), 'role ban')) {
+                    $triggeredBan = true;
+                }
+            }
         }
 
         return $this->render('tgdb/tickets/single.html.twig', [
             'ticket' => $ticket,
-            'badges' => array_filter(array_unique($badges))
+            'badges' => array_filter(array_unique($badges)),
+            'triggeredBan' => $triggeredBan
         ]);
     }
 }
