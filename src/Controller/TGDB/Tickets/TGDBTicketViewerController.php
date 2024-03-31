fcd4b0d89dc2eea738c2ce1d911e25cbf6a6d310
Nick <nick@nfreader.net>
2024-03-31 12:38:48 -0400

    Adds a rough words-per-minute count to ahelps

--- src/Controller/TGDB/Tickets/TGDBTicketViewerController.php
+++ src/Controller/TGDB/Tickets/TGDBTicketViewerController.php
@@ -5,6 +5,7 @@ namespace App\Controller\TGDB\Tickets;
 use App\Controller\Controller;
 use App\Domain\Ticket\Repository\TicketRepository;
 use App\Enum\TicketActions;
+use DateInterval;
 use Psr\Http\Message\ResponseInterface;
 use DI\Attribute\Inject;
 
@@ -18,13 +19,17 @@ class TGDBTicketViewerController extends Controller
         $ticket = $this->ticketRepository->getSingleTicket($this->getArg('round'), $this->getArg('ticket'))->getResults();
         $badges = [];
         $triggeredBan = false;
-        foreach($ticket as $t) {
+        foreach($ticket as $index => &$t) {
             $badges[] = $t->getSenderBadge();
             $badges[] = $t->getRecipientBadge();
             if(TicketActions::INTERACTION === $t->getAction()) {
                 if(str_contains($t->getMessage(), 'server ban') || str_contains($t->getMessage(), 'role ban')) {
                     $triggeredBan = true;
                 }
+            }
+            if($index > 0){
+                $duration = ($t->getTimestamp()->getTimestamp() - $ticket[$index-1]->getTimestamp()->getTimestamp()) / 60;
+                $t->wpm = round(str_word_count($t->getMessage())/$duration, 2);
             }
         }
 
